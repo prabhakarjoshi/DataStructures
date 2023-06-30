b3ac56afb289611c62cecbb4861867e04df972ee
sDEV-eloper <thisissurajdev@gmail.com>
2023-06-30 12:13:05 +0530

    module added

---
+++ arithmetic.js
@@ -0,0 +1,10 @@
+//arithmetic.js
+ const subtract=(a,b)=>a-b
+ const Mul=(a,b)=>a*b
+ const divide = (a, b) => a / b;
+ const modulo = (a, b) => a % b;
+ const sum=(a,b)=>a+b
+
+//module added
+
+module.exports={subtract, Mul, sum, divide, modulo}
